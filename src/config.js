2d125f6cb656bc74f781f6d440865c8c37b0e012
Franky Snow <FrankySnow@users.noreply.github.com>
2020-04-21 01:19:00 +0200

    set up aws amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-3",
+    BUCKET: "frankysnow1",
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://xeurmhh7ta.execute-api.eu-central-1.amazonaws.com/prod/",
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_SyEkmzeu9",
+    APP_CLIENT_ID: "5hgle1f1shvn1p9073a4q7jhsh",
+    IDENTITY_POOL_ID: "eu-central-1:f6fea2c8-8632-42e7-8924-b242af6a6194",
+  },
+}
